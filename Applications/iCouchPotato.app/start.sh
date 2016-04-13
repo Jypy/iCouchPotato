c346b68ddba908258339bb1f00ba6f2425081f4e
Jean-Philippe Bouillot <admin@jypy.eu>
2016-04-13 21:55:35 +0200

    Update 3.0.1

--- Applications/iCouchPotato.app/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-folder_app="/Applications/iCouchPotato.app/"
-folder_data="/var/mobile/Documents/JypyzProducts/iCouchPotato/"
-
-python ${folder_app}CouchPotato.py --quiet --daemon --data_dir $folder_data
